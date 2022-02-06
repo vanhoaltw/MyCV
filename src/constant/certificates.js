c1ba080e918340dd80395ac878d9a5a6a911beea
HoaNguyen <vanhoaltw@gmail.com>
2022-02-06 16:02:28 +0700

    done

--- src/constant/certificates.js
+++ src/constant/certificates.js
@@ -1,16 +1,9 @@
 export const certificates = [
     {
-        image: 'https://khbvptr.vn/wp-content/uploads/2019/10/certificate-la-gi.jpeg',
-        lession:'Frontend dev',
+        image: 'https://media-exp1.licdn.com/dms/image/C4E0BAQGLKj3JHcof0w/company-logo_200_200/0/1589990867649?e=1651708800&v=beta&t=RfABlgrPZsBF2-OJtk6BKF3yZ2d9aQ6BQU1QBcYqLe4',
+        lession:'Data Visualization',
         company:'Google',
-        date:new Date().toDateString(),
-        link:'http://google.com'
-    },
-    {
-        image: 'https://queenmaryacademy.org.uk/wp-content/uploads/2020/03/Certificate-Sample_page-0001.jpg',
-        lession:'Toeic',
-        company:'IIG Vietnam',
-        date:new Date().toDateString(),
-        link:'http://google.com'
+        date:"29/1/2022",
+        link:'https://freecodecamp.org/certification/fcc67e7d990-951f-4a94-b358-9bc6270832ab/data-visualization'
     }
 ]
