7e50e05f43bd19d43d280a992b58e619a16a82eb
HoaNguyen <vanhoaltw@gmail.com>
2022-02-10 17:43:08 +0700

    done

--- src/constant/certificates.js
+++ src/constant/certificates.js
@@ -2,8 +2,15 @@ export const certificates = [
     {
         image: 'https://media-exp1.licdn.com/dms/image/C4E0BAQGLKj3JHcof0w/company-logo_200_200/0/1589990867649?e=1651708800&v=beta&t=RfABlgrPZsBF2-OJtk6BKF3yZ2d9aQ6BQU1QBcYqLe4',
         lession:'Data Visualization',
-        company:'Google',
+        company:'FreeCodecamp',
         date:"29/1/2022",
         link:'https://freecodecamp.org/certification/fcc67e7d990-951f-4a94-b358-9bc6270832ab/data-visualization'
+    },
+    {
+        image: 'https://media-exp1.licdn.com/dms/image/C4E0BAQGLKj3JHcof0w/company-logo_200_200/0/1589990867649?e=1651708800&v=beta&t=RfABlgrPZsBF2-OJtk6BKF3yZ2d9aQ6BQU1QBcYqLe4',
+        lession:'Back End Development and APIs',
+        company:'FreeCodecamp',
+        date:"10/2/2022",
+        link:'https://www.freecodecamp.org/certification/fcc67e7d990-951f-4a94-b358-9bc6270832ab/back-end-development-and-apis'
     }
 ]
